99f46feab4a9c36ab9515e4935d490c149c51c6b
duwonders <duwonders@gmail.com>
2016-10-17 15:40:13 +0800

    搭建目录

---
+++ src/common/config/post.js
@@ -0,0 +1,8 @@
+export default {
+  json_content_type: ["application/json"],
+  max_file_size: 1024 * 1024 * 1024, //1G
+  max_fields: 100, 
+  max_fields_size: 2 * 1024 * 1024, //2M,
+  ajax_filename_header: "x-filename",
+  file_upload_path: "/Users/duzexuan/node/stumanage/runtime/upload",
+};
