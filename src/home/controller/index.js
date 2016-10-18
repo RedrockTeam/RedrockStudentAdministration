edc5b84677ea0a4c2812241af2a4686e3bec0fbd
duwonders <duwonders@gmail.com>
2016-10-18 19:04:20 +0800

    dsa

--- src/home/controller/index.js
+++ src/home/controller/index.js
@@ -1,81 +1,29 @@
 'use strict';
-
 import Base from './base.js'
-import redis from 'redis'
-import fs from 'fs'
-
-export default class extends Base {
-  /**
-   * index action
-   * @return {Promise} []
-   */
-  indexAction(){
-		this.session('userId', 2014213898)
-    return this.display()
-  }
-/**	
- * 上传接口
+/**
+ * 单入口模式,由route过滤以及分发请求
  */
-  async uploadAction(){
-		let	_redis = new  redis.createClient();
-		_redis.on("error", function (err) {
-				console.log("Error " + err);
-		});
-		/**
-		 * userId: 学号
-		 * filename: 文件名
-		 * filePath: tmp文件路径
-		 * fileTime: 上传文件次数
-		 */
-		let fileMessage = this.post(),
-				userId = await this.session('userId'),
-				fileName = fileMessage.name,
-				fileTime = fileMessage.time,
-				filePath = this.file(fileName).path,
-				uploadPath = think.RESOURCE_PATH + '/upload'
+/**	
+ * api列表:
+ * 
+ */
+export default class extends Base {
 
-				await fs.renameSync(filePath, `${uploadPath}/${fileName}`)
-				//将文件rename，防止被这个垃圾框架自动清除
+  indexAction(){
+		this.display()
+	}
 
-		//出口
-		if(fileMessage.complete){
-			_redis.hset(userId, 'complete', true)
-			this.success({complete:true})
+	async routeAction(){
+		let path = this.getPath()
+		let contoller = this.controller(path.role, 'home')
+		switch (contoller) {
+			case value:
+				
+				break;
+		
+			default:
+				break;
 		}
-		_redis.hset(userId, fileTime, `${uploadPath}/${fileName}`)
-		_redis.hset(userId, 'complete', false)
-		_redis.quit()
-		this.success();
-  }
-
-	readFilePromise(path){
-		return new Promise((resolve, reject) => {
-			fs.readFile(path, (err, chunk)=>{
-				if(err)
-					return reject(err)
-					resolve(chunk)
-			})
-		})
 	}
-
-	async taskAction(){
-		let	_redis = new  redis.createClient();
-		_redis.hgetall("2014213898", (err, res) => {
-			let content = []
-			for(let i in res){
-				if(i == "complete") continue
-				content[parseInt(i)] = this.readFilePromise(res[i]);
-			}
-			Promise.all(content)
-			.then((fileArr) => {
-				let completeFile = Buffer.concat(fileArr)
-				fs.writeFile(`/Users/duzexuan/node/test_dir/lalala.jpg`, completeFile, ()=>{
-					this.success();
-				})
-			})
-			.catch(err => {
-				console.log(err)
-			})
-		})
-	}
+	
 }
