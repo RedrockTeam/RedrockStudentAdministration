be7d3321571dda4289f0c2cde64beda566709409
duwonders <duwonders@gmail.com>
2016-12-10 15:48:16 +0800

    dsa

--- src/common/config/config.js
+++ src/common/config/config.js
@@ -5,6 +5,5 @@
 export default {
   route_on: true,
   pathname_prefix: "trainee", 
-  resource_on: true, // 是否处理静态资源请求， porxy_on 开启下可以关闭该配置
   resource_reg: /^((trainee\/)?static\/|[^\/]+\.(?!js|html)\w+$)/ //静态资源的正则
 };
