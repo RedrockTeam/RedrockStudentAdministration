c07766005c56f60f3550ae950c592d1a974abb8a
duwonders <duwonders@gmail.com>
2016-12-10 15:51:42 +0800

    dsa

---
+++ www/trainee/static/script/entry.es6.js
@@ -0,0 +1,55 @@
+'use strict';
+
+;(function () {
+    app.setRootLocation('/trainee/home/view');
+    var Router = app.Router;
+    var entryApp = document.getElementById('entryApp');
+    var loginRouter = new Router();
+    var userRouter = new Router();
+    var traineeIndexRouter;
+    var teacherIndexRouter;
+    var changeTitle = function () {
+        var navbarTitle = document.getElementById('navbar-title');
+        return function (val) {
+            navbarTitle.innerHTML = val;
+        };
+    }();
+    app.use(queryParserMiddleware());
+    app.use(routeHbsMiddleware(app.getRootLocation()));
+
+    loginRouter.route('/login', function (next) {
+        changeTitle('登陆');
+        this.render('/trainee/static/html/login.html', entryApp).then(function () {
+            next();
+        });
+    });
+    userRouter = userRouter.route('/user', function (next) {
+        if (!localStorage.getItem('userInf')) {
+            alert('请登录');
+            return app.redirect('', '/login');
+        };
+        next();
+    });
+    traineeIndexRouter = userRouter.route('/trainee', function (next) {
+        changeTitle('学员主页');
+        this.render('/trainee/static/html/trainee.html', entryApp).then(function () {
+            next();
+        });
+    });
+    teacherIndexRouter = userRouter.route('/teacher', function (next) {
+        changeTitle('管理员主页');
+        this.render('/trainee/static/html/teacher.html', entryApp).then(function () {
+            next();
+        });
+    });
+    traineeIndexRouter.route('/my_inf');
+    traineeIndexRouter.route('/my_work');
+    traineeIndexRouter.route('/my_lesson');
+    teacherIndexRouter.route('/people_administration');
+    teacherIndexRouter.route('/work_administration');
+    teacherIndexRouter.route('/watch_homework');
+    teacherIndexRouter.route('/lesson_administration');
+
+    window.traineeIndexRouter = traineeIndexRouter;
+    window.teacherIndexRouter = teacherIndexRouter;
+})();
